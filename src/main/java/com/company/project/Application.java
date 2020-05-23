eb01cba405345c4fe302e0fbce89b9d67daec8d4
shiqiao.liu <rgznlsqjy@163.com>
2020-05-23 20:58:28 +0800

    test

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -1,9 +1,11 @@
 package com.company.project;
 
+import tk.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.company.project.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
